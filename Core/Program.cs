7b09b2ccdbb0f475d147ca594310e3ac4da49971
YaeMonilc <Monilc@outlook.com>
2024-02-18 13:38:09 +0800

    Plugin support

--- Core/Program.cs
+++ Core/Program.cs
@@ -7,6 +7,11 @@ class Program
 {
     private static void Main(string[] args)
     {
+        ReflectionUtils.LoadDlls("./");
+        
+        CommandController.LoadCommands();
+        TranslatorController.LoadTranslators();
+        
         var argumentDictionary = ArgumentUtils.Parse(args);
         CommandController.Exec(argumentDictionary);
     }
